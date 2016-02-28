306fd2629d716395bb0039a368885c8e0c3306a4
Feihong Hsu <feihong.hsu@gmail.com>
2016-02-27 20:55:33 -0600

    Add script

---
+++ quickstart.py
@@ -0,0 +1,45 @@
+import base64
+from selenium import webdriver
+
+
+def main():
+    url = 'https://www.flickr.com/photos/diegojack/16400554073/'
+
+    browser = webdriver.Firefox()
+    browser.get(url)
+    browser.set_script_timeout(10)
+    data_uri = browser.execute_async_script(JAVASCRIPT)
+    with open('image.png', 'wb') as fp:
+        fp.write(base64.decodestring(data_uri))
+
+
+JAVASCRIPT = """
+var maxDimensions = 0;
+var biggestImage = null;
+for (var img of document.querySelectorAll('img')) {
+    var dim = img.naturalWidth * img.naturalHeight;
+    if (dim > maxDimensions) {
+        maxDimensions = dim;
+        biggestImage = img;
+    }
+}
+// Convert image to base64.
+var resolve = arguments[arguments.length - 1];
+//resolve('foobar')
+var img = new Image();
+img.crossOrigin = 'Anonymous';
+img.onload = function() {
+    var canvas = document.createElement('CANVAS');
+    canvas.height = this.height;
+    canvas.width = this.width;
+    canvas.getContext('2d').drawImage(this, 0, 0);
+    data = canvas.toDataURL('image/png').replace(/^data:image\/(png|jpg);base64,/, '');
+    canvas = null;
+    resolve(data);
+};
+img.src = biggestImage.src;
+"""
+
+
+if __name__ == '__main__':
+    main()
